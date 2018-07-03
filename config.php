47aab2205869dd9124549e735b01e2db29e6020f
boypro477 <nguyenanhtu0407@gmail.com>
2018-07-03 19:39:29 +0700

    add database

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "mycoffee";
+$hostname = "sql9.freemysqlhosting.net";
+$dbuser = "sql9245876";
+$dbpass = "2RCBzQ9YDT";
+$dbname = "sql9245876";
 ?>
