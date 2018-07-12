1d7f88da4378e1162347164de2e536d77bc7ed6b
boypro477 <nguyenanhtu0407@gmail.com>
2018-07-12 22:06:37 +0700

    change server db

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = "databases.000webhost.com";
-$dbuser = "id6470429_admin";
-$dbpass = "admin";
-$dbname = "id6470429_mycoffee";
+$hostname = "ec2-54-221-198-206.compute-1.amazonaws.com";
+$dbuser = "fuhaxfezneyjqm";
+$dbpass = "8c5ea277aeef9de48941eacbc4e1e622bc6c07af9315fdf295d3bc505804d34f";
+$dbname = "dfguq3q8pra9qv";
 ?>
